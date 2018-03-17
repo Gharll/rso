de8acb920bfb992e1f67aa28c689ca22dce39f87
Gharll <mrprzemob@gmail.com>
2018-03-17 17:03:28 +0100

    the number is send to server and understand that data

--- Zad1/netclient.c
+++ Zad1/netclient.c
@@ -1,3 +1,5 @@
+/*  Make the necessary includes and set up the variables.  */
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -5,33 +7,51 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "message.c"
 
-int main()
+int main ()
 {
     int sockfd;
     socklen_t len;
     struct sockaddr_in address;
     int result;
-    char ch = 'A';
+    message m;
 
+    /*  Create a socket for the client.  */
     sockfd = socket (AF_INET, SOCK_STREAM, 0);
 
+    /*  Name the socket, as agreed with the server.  */
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
-    address.sin_port = htons(9734);
-    len = sizeof(address);
-    result = connect(sockfd, (struct sockaddr *) &address, len);
+    address.sin_addr.s_addr = inet_addr ("127.0.0.1");
+    address.sin_port = htons (9734);
+    len = sizeof (address);
 
-    if(result == -1){
-        perror("oops: netclient");
-        exit(1);
+    /*  Now connect our socket to the server's socket.  */
+    result = connect (sockfd, (struct sockaddr *) &address, len);
+
+    if (result == -1)
+    {
+        perror ("oops: netclient");
+        exit (1);
     }
 
-    write(sockfd, &ch, 1);
-    read(sockfd, &ch, 1);
+    /*  We can now read/write via sockfd.  */
+    byte code [] = {'0','0','0','1'};
+    write_message_code(&m, code);
+    write_rq_id(&m, 100);
+    write_number(&m, 2.35);
 
-    printf("The end of program\n");
-    close(sockfd);
+    byte * buffor = serialize_message(&m);
 
+    write (sockfd, buffor, get_message_size(&m));
+
+    read (sockfd, &buffor, get_message_size(&m));
+
+    message response;
+    byte * buffor2 = (byte*) malloc(sizeof(message));
+    deserialize_message(&response, buffor2);
+
+    printf ("char from server = %f\n", get_number(&response));
+    close (sockfd);
     exit (0);
 }
--- Zad1/server.c
+++ Zad1/server.c
@@ -5,57 +5,75 @@
 #include <netinet/in.h>
 #include <signal.h>
 #include <unistd.h>
-int main ()
+#include "message.c"
+
+int
+main ()
 {
     int server_sockfd, client_sockfd;
     socklen_t server_len, client_len;
     struct sockaddr_in server_address;
     struct sockaddr_in client_address;
+
     server_sockfd = socket (AF_INET, SOCK_STREAM, 0);
+
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl (INADDR_ANY);
     server_address.sin_port = htons (9734);
     server_len = sizeof (server_address);
-    bind (server_sockfd, (struct sockaddr *)
-          &server_address, server_len);
-    /*  Create a connection queue, ignore child exit
-    details and wait for clients.  */
+    bind (server_sockfd, (struct sockaddr *) &server_address, server_len);
+
+    /*  Create a connection queue, ignore child exit details and wait for clients.  */
+
     listen (server_sockfd, 5);
+
     signal (SIGCHLD, SIG_IGN);
+
     while (1)
     {
-        char ch;
+        char ch[4];
+        message m;
+        int message_len = get_message_size(&m);
+
+        byte * buffor = (byte*)malloc(message_len);
+
         printf ("server waiting\n");
+
         /*  Accept connection.  */
+
         client_len = sizeof (client_address);
         client_sockfd = accept (server_sockfd,
-                                (struct sockaddr *)
-                                &client_address,
+                                (struct sockaddr *) &client_address,
                                 &client_len);
-        /*  Fork to create a process for this client
-        and perform a test to see
-        whether we're the parent or the child.
-        */
+
+        /*  Fork to create a process for this client and perform a test to see
+            whether we're the parent or the child.  */
+
         if (fork () == 0)
         {
-            /*  If we're the child, we can now
-            read/write to the client on client_sockfd.
-            The five second delay is just
-            for this demonstration.  */
-            read (client_sockfd, &ch, 1);
-            sleep (5);
-            ch++;
-            write (client_sockfd, &ch, 1);
+
+            /*  If we're the child, we can now read/write to the client on client_sockfd.
+                The five second delay is just for this demonstration.  */
+
+
+            read (client_sockfd, buffor, message_len);
+
+            deserialize_message(&m, buffor);
+            double number = get_number(&m);
+            int rq_id = get_rq_id(&m);
+            printf("The id is: %d. The number is: %f", rq_id, number);
+
+
+            write (client_sockfd, &ch, 4 * sizeof(char));
             close (client_sockfd);
             exit (0);
         }
-        /*  Otherwise, we must be the parent and our
-        work for this client is finished.  */
+
+            /*  Otherwise, we must be the parent and our work for this client is finished.  */
+
         else
         {
             close (client_sockfd);
         }
-
-        printf("The end of loop");
     }
 }
