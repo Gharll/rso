6973f077b11fcb685a021d91002e6ca1634ab2c1
Gharll <mrprzemob@gmail.com>
2018-03-18 17:01:04 +0100

    working on single request to server. Time request is prototype

--- Zad1/main.c
+++ Zad1/main.c
@@ -1,8 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "message.c"
+
+
+
 
 int main()
 {
-    printf("Hello world!\n");
+    char a[] = "abc";
+    char * b = a+2;
+
+    printf("%s", b);
+    /*
+     message m;
+     write_number(&m, 2.34);
+     byte * buffor = serialize_message(&m);
+
+     message m2;
+     deserialize_message(&m2, buffor);
+
+     printf("%f", get_number(&m2));
+
+      byte a = 1;
+
+     double number = 5.23;
+     number /= 3;
+     number *= 10000000000000;
+
+     printf("Number: %f\n", number);
+     printf("Size = %d\n",sizeof(byte));
+
+     message m;
+
+
+
+     char buffer[] = "jakis napis";
+     char *p = buffer;
+     p+=2;
+
+     //buffer -= 2*sizeof(char);
+
+     printf("%c", *p);
+
+     printf("%d \n", sizeof(message));
+     int my_size = 4 * sizeof(byte) +  sizeof(byte) * sizeof(int) + sizeof(byte) * sizeof(double);
+     printf("%d \n", my_size);
+
+     char napis[] = "jakis napis";
+     char to_cpy[2] = "ko";
+
+     memset(napis +2, to_cpy, sizeof(to_cpy));
+     puts(napis);
+
+      */
+/*
+
+    int a = 88;
+    int *ptr = &a;
+    double d1 = 55;
+
+    byte rq_id[sizeof(int)];
+    byte number[sizeof(double)];
+    byte * message = (byte*) malloc(sizeof(int) + sizeof(double));
+    byte *buffor = message;
+
+    memcpy(rq_id, ptr, sizeof(int));
+    memcpy(buffor, rq_id, sizeof(int));
+    buffor += sizeof(int);
+
+    memcpy(number, &d1, sizeof(double));
+    memcpy(buffor, number, sizeof(double));
+    buffor += sizeof(double);
+
+    int b = -1;
+    double d2 = -1;
+
+    buffor -= sizeof(int) + sizeof(double);
+    memcpy(&b, buffor, sizeof(int));
+
+    buffor += sizeof(int);
+    memcpy(&d2, buffor, sizeof(double));
+
+    printf("%d\n", b);
+    printf("%f\n", d2);
+
+
+   /* int a = 5;
+    byte s[sizeof(int)];
+    int b;
+
+    memcpy(s, &a, sizeof(int));
+    memcpy(&b, s, sizeof(int));
+
+    printf("%d", b); */
+
     return 0;
 }
--- Zad1/server.c
+++ Zad1/server.c
@@ -5,10 +5,10 @@
 #include <netinet/in.h>
 #include <signal.h>
 #include <unistd.h>
+#include <math.h>
 #include "message.c"
 
-int
-main ()
+int main ()
 {
     int server_sockfd, client_sockfd;
     socklen_t server_len, client_len;
@@ -32,10 +32,7 @@ main ()
     while (1)
     {
         char ch[4];
-        message m;
-        int message_len = get_message_size(&m);
 
-        byte * buffor = (byte*)malloc(message_len);
 
         printf ("server waiting\n");
 
@@ -45,7 +42,7 @@ main ()
         client_sockfd = accept (server_sockfd,
                                 (struct sockaddr *) &client_address,
                                 &client_len);
-
+        printf("Accepted connection\n");
         /*  Fork to create a process for this client and perform a test to see
             whether we're the parent or the child.  */
 
@@ -54,17 +51,67 @@ main ()
 
             /*  If we're the child, we can now read/write to the client on client_sockfd.
                 The five second delay is just for this demonstration.  */
+            printf("forked\n");
 
-
+            
+            message request;
+            int message_len = get_message_size();
+            byte * buffor = (byte*)malloc(message_len);
             read (client_sockfd, buffor, message_len);
+            deserialize_message(&request, buffor);
 
-            deserialize_message(&m, buffor);
-            double number = get_number(&m);
-            int rq_id = get_rq_id(&m);
-            printf("The id is: %d. The number is: %f", rq_id, number);
+            char * message_code = get_message_code(&request);
+            printf("The message code: %s\n", message_code);
+            if(strcmp(message_code, SQRT_REQUEST_CODE) == 0){
+                int id = get_rq_id(&request);
+                double number = get_number(&request);
+                double sqrt_number = sqrt(number);
+
+                message response;
+                byte code [4] = SQRT_RESPONSE_CODE;
+                write_message_code(&response, code);
+                write_rq_id(&response, get_rq_id(&request));
+                write_number(&response, sqrt_number);
+
+                byte * buffor = serialize_message(&response);
+                /*message test;
+                deserialize_message(&test, buffor);
+                printf("%s, %d, %f \n", get_message_code(&test), get_rq_id(&test), get_number(&test));
+                */
+                write (client_sockfd, buffor, get_message_size());
+                printf("Send sqrt response\n");
+            }
+
+            if(strcmp(message_code, TIME_REQUEST_CODE) == 0){
+                int id = get_rq_id(&request);
+
+                message response;
+                byte code [4] = TIME_RESPONSE_CODE;
+                write_message_code(&response, code);
+                write_rq_id(&response, get_rq_id(&request));
+                write_length(&response, 3);
+                write_time(&response, "abc", 3);
+
+                byte * buffor = serialize_message(&response);
+
+                /*message test;
+                deserialize_message(&test, buffor);
+                int len = get_length(&test);
+                char * msg = get_time(&test, len);
+
+                printf("Len: %d\n", len);
 
 
-            write (client_sockfd, &ch, 4 * sizeof(char));
+                printf("Msg: %s\n", msg);*/
+
+                write (client_sockfd, buffor, get_message_size());
+                free(buffor);
+                printf("Send time response\n");
+
+            }
+
+
+            printf("Close connection\n");
             close (client_sockfd);
             exit (0);
         }
